e64fbf3e4a231e4333d78eb65cb0794056180a0c
john-nkzw <y.nakazw@gmail.com>
2018-10-01 21:06:11 +0900

    ver.2
    D0003 commit

--- nkzw_github/D003.py
+++ nkzw_github/D003.py
@@ -1,13 +1,8 @@
-# coding: utf-8
-# Your code here!
 num = input()
 input_lines = int(num)
-a=2*input_lines
-b=3*input_lines
-c=4*input_lines
-d=5*input_lines
-e=6*input_lines
-f=7*input_lines
-g=8*input_lines
-h=9*input_lines
-print('%d %d %d %d %d %d %d %d %d' %(input_lines,a,b,c,d,e,f,g,h))
+list = [str(input_lines)]
+for i in range(2,10):
+    a = input_lines*i
+    list.append(str(a))
+    
+print(' '.join(list))
