b3769551af7acfa4a98ba615f53147e87c8ba58a
john-nkzw <y.nakazw@gmail.com>
2018-10-01 20:35:18 +0900

    hoge
    D003 commit

---
+++ nkzw_github/D003.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# Your code here!
+num = input()
+input_lines = int(num)
+a=2*input_lines
+b=3*input_lines
+c=4*input_lines
+d=5*input_lines
+e=6*input_lines
+f=7*input_lines
+g=8*input_lines
+h=9*input_lines
+print('%d %d %d %d %d %d %d %d %d' %(input_lines,a,b,c,d,e,f,g,h))
